69f3a8d90ffed46302fedae61ebff19802c9b0ed
suniantaraa <agungsuniantara@gmail.com>
2023-07-31 11:19:59 +0800

    update

---
+++ loop.py
@@ -0,0 +1,5 @@
+angka = 10
+angka2 = 0
+
+for n in range(angka):
+    for angka2
---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+
+nilai1 = math.sqrt(pow(5, 2))
+nilai2 = 15 + 1
+nilai3 = 2 * pow(15, 2)
+
+print((nilai1 / nilai2) + nilai3)
+
+nilai4 = -8 + math.sqrt(pow(8, 2) - (4 * 1 * 12))
+nilai5 = 2 * 1
+
+print(nilai4 / nilai5)
+
+nilai6 = -8 - math.sqrt(pow(8, 2) - (4 * 1 * 12))
+nilai7 = 2*1
+
+print(nilai6 / nilai7)
